35e16de014d826936240aa56557ce0c01a14f533
baba-s <shotababa1206@gmail.com>
2023-02-01 21:05:51 +0900

    add

---
+++ Editor/SetXcodeCcacheOnPostProcessBuild.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+namespace Kogane
+{
+    /// <summary>
+    /// iOS ビルド完了時に Xcode プロジェクトに ccache を設定するエディタ拡張
+    /// https://zenn.dev/pobo380/articles/1a5d838ee857e1
+    /// </summary>
+    public sealed class SetXcodeCcacheOnPostProcessBuild
+    {
+        //================================================================================
+        // プロパティ(static)
+        //================================================================================
+        public static Func<bool> OnIsEnable { get; set; } = () => true;
+
+        //================================================================================
+        // 関数(static)
+        //================================================================================
+        /// <summary>
+        /// ビルド完了時に呼び出されます
+        /// </summary>
+        [PostProcessBuild]
+        private static void OnPostProcessBuild
+        (
+            BuildTarget buildTarget,
+            string      pathToBuiltProject
+        )
+        {
+            if ( buildTarget != BuildTarget.iOS ) return;
+            if ( OnIsEnable == null || !OnIsEnable() ) return;
+
+            const string guid = "b39433204aac24fadb214e7318dfad8b";
+
+            var assetPath = AssetDatabase.GUIDToAssetPath( guid );
+
+            File.Copy
+            (
+                sourceFileName: assetPath,
+                destFileName: $"{pathToBuiltProject}/ccache_wrapper",
+                overwrite: true
+            );
+
+            var projectPath = PBXProject.GetPBXProjectPath( pathToBuiltProject );
+            var project     = new PBXProject();
+
+            project.ReadFromFile( projectPath );
+
+            var targetGuid = project.ProjectGuid();
+
+            project.AddBuildProperty( targetGuid, "CC", "$(PROJECT_DIR)/ccache_wrapper" );
+            project.WriteToFile( projectPath );
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.SetXcodeCcacheOnPostProcessBuild
+# Kogane Set Xcode Ccache On Post Process Build
+
+iOS ビルド完了時に Xcode プロジェクトに ccache を設定するエディタ拡張
